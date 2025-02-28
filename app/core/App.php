0a777073f4a545e0c4579f0ef7c6ab8b97b2a76c
mukhliskhoirudin-ofc <mukhliskhoirudin.official@gmail.com>
2025-02-28 15:10:05 +0700

    feat: routing

--- app/core/App.php
+++ app/core/App.php
@@ -4,6 +4,17 @@ class App
 {
     public function __construct()
     {
-        echo 'OK!';
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL()
+    {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');     //rtrim = untuk hilangkan tanda sles
+            $url = filter_var($url, FILTER_SANITIZE_URL);   //bersihkan url dari karakter yang aneh biar aman
+            $url = explode('/', $url);  //pecah url, nanti slase hilang, dan string jadi array
+            return $url;
+        }
     }
 }
