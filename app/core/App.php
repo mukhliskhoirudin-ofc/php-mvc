e6185386d489f56d58573f1d91c190409de7d54c
mukhliskhoirudin-ofc <mukhliskhoirudin.official@gmail.com>
2025-03-01 16:05:53 +0700

    feat: controller

--- app/core/App.php
+++ app/core/App.php
@@ -2,10 +2,42 @@
 
 class App
 {
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
+
     public function __construct()
     {
         $url = $this->parseURL();
-        var_dump($url);
+
+        //controller
+        if (file_exists('../app/controllers/' . $url[0] . '.php')) { //cek ada gk file di folder contrlr sesuai dgn url yg ditulis
+            //kalo ada timpa, maka akan menjadi controller yang baru
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+
+        require_once '../app/controllers/' . $this->controller . '.php';    //lalu panggil contoller barunya
+        $this->controller = new $this->controller;
+
+
+        //method
+        if (isset($url[1])) {   //kalau ada method
+            if (method_exists($this->controller, $url[1])) { //cek methodnya ada gk di contoller
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+
+        //params
+        if (!empty($url)) {
+            $this->params = array_values($url); // ambil datanya gunakan array_values
+        }
+
+
+        //jalanlan controller & method, serta kirimkan params jika ada gunakan call_user_func_array
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
     public function parseURL()
@@ -16,5 +48,7 @@ class App
             $url = explode('/', $url);  //pecah url, nanti slase hilang, dan string jadi array
             return $url;
         }
+
+        return [0];
     }
 }
